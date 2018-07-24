a25d407246031230f96e18c07f1b0e7fa29d1612
Fabien Le Houëdec <fabien.lehouedec@gmail.com>
2018-07-24 15:58:16 +0200

    Merge between controllers.units with develop

--- AspNetGame/Models/IdentityModels.cs
+++ AspNetGame/Models/IdentityModels.cs
@@ -50,5 +50,23 @@ namespace AspNetGame.Models
         public System.Data.Entity.DbSet<AspNetGame.Models.Game.Stores.GoldWarehouse> GoldWarehouses { get; set; }
 
         public System.Data.Entity.DbSet<AspNetGame.Models.Game.Stores.IronWarehouse> IronWarehouses { get; set; }
+
+        public System.Data.Entity.DbSet<AspNetGame.Models.Game.Ships.Cruiser> Cruisers { get; set; }
+
+        public System.Data.Entity.DbSet<AspNetGame.Models.Game.Ships.Striker> Strikers { get; set; }
+
+        public System.Data.Entity.DbSet<AspNetGame.Models.Game.Ships.Destroyer> Destroyers { get; set; }
+
+        public System.Data.Entity.DbSet<AspNetGame.Models.Game.Defence.PowerShield> PowerShields { get; set; }
+
+        public System.Data.Entity.DbSet<AspNetGame.Models.Game.Defence.LaserTower> LaserTowers { get; set; }
+
+        public System.Data.Entity.DbSet<AspNetGame.Models.Game.Defence.MissileLauncher> MissileLaunchers { get; set; }
+
+        public System.Data.Entity.DbSet<AspNetGame.Models.Game.Factories.SpaceShipyard> SpaceShipyards { get; set; }
+
+        public System.Data.Entity.DbSet<AspNetGame.Models.Game.Factories.ResearchCenter> ResearchCenters { get; set; }
+
+        public System.Data.Entity.DbSet<AspNetGame.Models.Game.Factories.WeaponFactory> WeaponFactories { get; set; }
     }
 }
