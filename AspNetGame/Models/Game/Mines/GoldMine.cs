4f79205061db16a4f2a60cbf997ac9944c01e804
masteryodassin <yves-mael.bunetel@hotmail.fr>
2018-07-24 10:55:22 +0200

    commit Stores and Mines

---
+++ AspNetGame/Models/Game/Mines/GoldMine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetGame.Models.Game.Mines
+{
+    public class GoldMine : IGoldExtractor
+    {
+
+        long IExtractor.Extraction()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AspNetGame/Models/Game/Stores/IStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetGame.Models.Game.Stores
+{
+    public interface IStore
+    {
+        long Capacity ();
+    }
+}
