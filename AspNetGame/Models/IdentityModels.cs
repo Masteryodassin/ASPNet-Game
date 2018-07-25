b75691d96a219295d3c498cef50c46e2acdfac9f
Fabien Le Houëdec <fabien.lehouedec@gmail.com>
2018-07-25 12:24:16 +0200

    Adding some test data at db creation

--- AspNetGame/Models/IdentityModels.cs
+++ AspNetGame/Models/IdentityModels.cs
@@ -1,4 +1,6 @@
-﻿using System.Security.Claims;
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -29,17 +31,49 @@ namespace AspNetGame.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+
         public ApplicationDbContext()
             : base("AuthenticationConnection", throwIfV1Schema: false)
         {
-            this.Database.CreateIfNotExists();
+            if (this.Database.CreateIfNotExists())
+            {
+                Seed(this);
+            }
             if (!this.Database.CompatibleWithModel(false))
             {
                 this.Database.Delete();
-                this.Database.CreateIfNotExists();
+                if (this.Database.CreateIfNotExists())
+                {
+                    Seed(this);
+                }
             }
         }
-        
+
+        private void Seed(ApplicationDbContext context)
+        {
+            List<IdentityRole> defaultRoles = new List<IdentityRole>
+            {
+                new IdentityRole() { Name = "Admin" },
+                new IdentityRole() { Name = "Employee" },
+                new IdentityRole() { Name = "Manager" }
+            };
+
+            //INSERT INTO [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], 
+            //[LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) VALUES 
+            //(N'53c5b44c-5208-4319-804b-11bcac7a5aa2', N'fabien.lehouedec@gmail.com', 0, N'AACws3iAjLap/fFWVxHc1/47Fo8p9FOOILqEHo1weeHfRuUkaP1ZKPAL1jQYSca8XQ==',
+            //N'359c4600-4b9e-4588-9556-b107ad06b164', NULL, 0, 0, NULL, 1, 0, N'fabien')
+
+
+           // IdentityUser adminUser = new IdentityUser() { Email = "admin@mywebsite.com", PasswordHash =  }
+
+            foreach (IdentityRole role in defaultRoles)
+            {
+                context.Roles.Add(role);
+            }
+
+            context.SaveChanges();
+        }
+
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
